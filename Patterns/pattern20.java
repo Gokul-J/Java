3fc6db04f69f7c3ab806b312f65f1d37ad376971
Gokul-J <jgokul2000@gmail.com>
2020-09-24 09:14:25 +0530

    Updated patttern

---
+++ Patterns/pattern20.java
@@ -0,0 +1,26 @@
+/*
+Input : 5
+Pattern:
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+*/
+
+import java.util.Scanner;
+public class pattern20 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int i=n; i>0; i--){
+      for(int j=n; j>i; j--){
+        System.out.print(" ");
+      }
+      for(int j=0; j<i; j++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
